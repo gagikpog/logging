6acafe0b4fb897a97a3e9c9790ca59ebb25ae878
Gagik <gagikpog@gmail.com>
2024-11-30 11:24:06 +0500

    ad header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Home from './pages/home';
 import Auth from './pages/auth';
 import { Fragment } from 'react';
@@ -7,20 +7,31 @@ import PrivateRoute from './components/privateRoute';
 import { Provider as UserProvider } from './contexts/user';
 import Apps from './pages/apps';
 import NotFound from './pages/notFound';
+import Nav from './components/nav';
 
 function App() {
     return (
         <UserProvider>
-            <Router>
-                <Fragment>
-                    <Routes>
-                        <Route path="/" element={<PrivateRoute><Home /></PrivateRoute>}/>
-                        <Route path="/auth/" element={ <Auth />}/>
-                        <Route path="/apps/" element={ <Apps />}/>
-                        <Route path="*" element={ <NotFound />}/>
-                    </Routes>
-                </Fragment>
-            </Router>
+            <>
+                <Router>
+                    <Nav />
+                    <Fragment>
+                        <Routes>
+                            <Route
+                                path="/"
+                                element={
+                                <PrivateRoute>
+                                    <Home />
+                                </PrivateRoute>
+                                }
+                            />
+                            <Route path="/auth/" element={<Auth />} />
+                            <Route path="/apps/" element={<Apps />} />
+                            <Route path="*" element={<NotFound />} />
+                        </Routes>
+                    </Fragment>
+                </Router>
+            </>
         </UserProvider>
     );
 }
